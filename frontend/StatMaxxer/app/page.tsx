276e4d9a8c26883901eb6e0df1087ed0526642a7
BaneofRogue <whkeller.dev@gmail.com>
2026-03-25 14:06:15 -0400

    blank

---
+++ frontend/StatMaxxer/app/page.tsx
@@ -0,0 +1,12 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.page}>
+      <h1 className={styles.title}>StatMaxxer</h1>
+      <p className={styles.subtitle}>
+        Blank lol
+      </p>
+    </main>
+  );
+}
