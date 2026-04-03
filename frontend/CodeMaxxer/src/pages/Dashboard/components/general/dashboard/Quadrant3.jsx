6f402509056b7545a2328303168f8a97d603ccb4
BaneofRogue <whkeller.dev@gmail.com>
2026-04-02 20:48:09 -0400

    quadrant scaff

---
+++ frontend/CodeMaxxer/src/pages/Dashboard/components/general/dashboard/Quadrant3.jsx
@@ -0,0 +1,5 @@
+import styles from './Quadrant3.module.css'
+
+export default function Quadrant3() {
+    return <div className={styles.root} />
+}
---
+++ frontend/CodeMaxxer/src/pages/Dashboard/components/general/dashboard/Quadrant4.jsx
@@ -0,0 +1,13 @@
+import styles from './Quadrant4.module.css'
+
+export default function Quadrant4() {
+    return (
+        <div className={styles.root}>
+            <div className={styles.stack}>
+                <div className={styles.stackItem} />
+                <div className={styles.stackItem} />
+                <div className={styles.stackItem} />
+            </div>
+        </div>
+    )
+}
